1b9f5c9d81e5720025cdfa265f72ba489f6456c0
Eduardo <eduardofelipe_2000@hotmail.com>
2023-10-11 08:40:41 -0300

    corrigindo erros

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 "use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return `Olá, ${nome}`;
+}
+const resultadoMultiplicacao = multiplicar(3, 4);
+const mensagemSaudacao = saudacao('Eduardo');
+console.log(resultadoMultiplicacao);
+console.log(mensagemSaudacao);
